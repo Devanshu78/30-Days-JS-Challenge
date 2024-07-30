3e2a174756e09209c00d33ca427370e9e71990d5
Devanshu <devanshupandey281@gmail.com>
2024-07-31 01:34:29 +0530

    Day-01 Variables

--- Day-2 Operators/feature_3.js
+++
@@ -1,4 +0,0 @@
-let num = Math.floor(Math.random() * 10);
-
-console.log(num);
-num >= 0 ? console.log("+ve") : console.log("-ve");
--- Day-3 Control Structures/feature_2.js
+++
@@ -1,6 +0,0 @@
-let age = Math.floor(Math.random() * 100);
-if (age >= 18) {
-  console.log("You are is " + age + ". You can vote");
-} else {
-  console.log("Your age is " + age + ". you can't vote");
-}
